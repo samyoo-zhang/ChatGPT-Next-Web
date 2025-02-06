f54098700cf08933f3dbb9ee847017469e536bd8
samyoo-zhang <804388774@qq.com>
2025-02-07 00:33:49 +0800

    feat: add custom gaId

--- app/constant.ts
+++ app/constant.ts
@@ -561,7 +561,8 @@ export const internalAllowedWebDavEndpoints = [
   "https://app.koofr.net/dav/Koofr",
 ];
 
-export const DEFAULT_GA_ID = "G-89WN60ZK2E";
+// export const DEFAULT_GA_ID = "G-89WN60ZK2E";
+export const DEFAULT_GA_ID = "G-WV7Q1F1F1B";
 export const PLUGINS = [
   { name: "Plugins", path: Path.Plugins },
   { name: "Stable Diffusion", path: Path.Sd },
