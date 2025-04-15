c9be2b1d17e06d0549e45111586212d7e26012ce
samyoo-zhang <804388774@qq.com>
2025-04-15 10:49:10 +0800

    feat: 优化a标签点击

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,28 @@ export const metadata: Metadata = {
   },
 };
 
+document.addEventListener("click", function (event) {
+  // 检查点击的目标是否是 <a> 标签
+  if (event.target?.tagName === "A") {
+    // 阻止默认行为（跳转）
+    event.preventDefault();
+
+    // 获取 <a> 标签的 href 属性
+    const href = event.target?.getAttribute("href");
+    const url = decodeURIComponent(href);
+    // 自定义处理逻辑
+    console.log("拦截到链接点击:", href, url);
+
+    if (url.startsWith("#")) {
+      // 锚点滚动
+      document.querySelector(url)?.scrollIntoView();
+    } else {
+      // 如果需要跳转，可以手动处理
+      window.open(url);
+    }
+  }
+});
+
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
